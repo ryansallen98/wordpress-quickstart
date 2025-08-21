f8583cd0f598fcae0d9b1e12a59b888051bca698
Ryan Allen <ryansallen53@gmail.com>
2025-08-21 18:52:06 +0100

    feat: auto load files from includes folder

--- functions.php
+++ functions.php
@@ -5,3 +5,16 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 // Initialize Timber.
 Timber\Timber::init();
+
+// Define constants.
+define("MY_THEME_INC", get_template_directory() . "/includes");
+
+// Auto-load all PHP files in /includes and its subfolders.
+$iterator = new RecursiveIteratorIterator(
+    new RecursiveDirectoryIterator(MY_THEME_INC),
+);
+foreach ($iterator as $file) {
+    if ($file->isFile() && $file->getExtension() === "php") {
+        require_once $file->getPathname();
+    }
+}
