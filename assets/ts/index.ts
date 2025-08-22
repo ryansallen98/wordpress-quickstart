2cf1e83a61dfeacf7a238fb61dbae58437762445
Ryan Allen <ryansallen53@gmail.com>
2025-08-22 12:09:54 +0100

    feat: initialize Alpine.js and declare global types in index.ts

--- assets/ts/index.ts
+++ assets/ts/index.ts
@@ -1,1 +1,20 @@
+// Import styles
 import "../styles/index.css";
+
+// Import node modules
+import Alpine from "alpinejs";
+import "htmx.org";
+
+// Declare global types
+declare global {
+  interface Window {
+    Alpine: typeof Alpine;
+    htmx: any;
+  }
+}
+
+// Assign Alpine to the window object
+window.Alpine = Alpine;
+
+// Initialize Alpine
+Alpine.start();
