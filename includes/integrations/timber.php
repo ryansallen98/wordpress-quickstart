fdfe828ca2ece7297fdb7041b0e457184670709a
Ryan Allen <ryansallen53@gmail.com>
2025-08-23 22:15:04 +0100

    feat: expose tailwind-merge to Twig with filter and function support

--- includes/integrations/timber.php
+++ includes/integrations/timber.php
@@ -7,3 +7,35 @@ if (!defined('ABSPATH')) {
 
 // Initialize Timber.
 Timber\Timber::init();
+
+/**
+ * Expose tailwind-merge to Twig as:
+ *  - filter:   {{ 'px-2 px-4'|twmerge }}
+ *  - function: {{ twmerge(['px-2', 'px-4']) }}
+ *
+ * Requires: composer require gehrisandro/tailwind-merge-php
+ */
+add_filter('timber/twig', function (\Twig\Environment $twig) {
+    // Try to resolve a TailwindMerge instance from common namespaces.
+    $merge = null;
+
+    if (class_exists('\TailwindMerge\TailwindMerge')) {
+        $merge = \TailwindMerge\TailwindMerge::instance();
+    } elseif (class_exists('\TitasGailius\TailwindMerge\TailwindMerge')) {
+        // Example of another possible namespace – adjust if your package differs.
+        $merge = \TitasGailius\TailwindMerge\TailwindMerge::instance();
+    } elseif (class_exists('\TailwindMerge')) {
+        // If the class really is global.
+        $merge = \TailwindMerge::instance();
+    }
+
+    // Callable used by both filter and function.
+    $callable = $merge
+        ? static fn($classes) => $merge->merge($classes)
+        : static fn($classes) => $classes; // no-op so Twig never errors
+
+    $twig->addFilter(new \Twig\TwigFilter('twmerge', $callable));
+    $twig->addFunction(new \Twig\TwigFunction('twmerge', $callable));
+
+    return $twig;
+});
