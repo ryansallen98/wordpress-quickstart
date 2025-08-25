d98579103bacc76c0d15dd32c0d0cb02bea9262a
Ryan Allen <ryansallen53@gmail.com>
2025-08-25 19:12:03 +0100

    feat: add theme support for menus in setup.php

--- includes/core/setup.php
+++ includes/core/setup.php
@@ -5,6 +5,7 @@ add_action('after_setup_theme', function () {
     add_theme_support("title-tag");
     add_theme_support("post-thumbnails");
     add_theme_support("html5", ["search-form", "gallery", "caption"]);
+    add_theme_support('menus');
 
     remove_theme_support('block-templates');
 });
