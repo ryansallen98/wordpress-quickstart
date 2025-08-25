4703ef3b79ab2aab9b0e8848c9b50e29c2baee08
Ryan Allen <ryansallen53@gmail.com>
2025-08-25 10:50:32 +0100

    refactor: simplify footer rendering by using Timber context directly

--- footer.php
+++ footer.php
@@ -1,10 +1,4 @@
 <?php
 
-$timberContext = $GLOBALS['timberContext'];
-if (!isset($timberContext)) {
-    throw new \Exception('Timber context not set in footer.');
-}
-$timberContext['content'] = ob_get_contents();
-ob_end_clean();
-$templates = array('page-plugin.twig');
-Timber::render($templates, $timberContext);
+$context = Timber::context();
+Timber::render('layouts/footer.twig', $context);
