6060a7538a6765fecbb503bce083855d5a11b17b
Ryan Allen <ryansallen53@gmail.com>
2025-08-30 20:17:20 +0100

    feat: refactor accordion component to use Alpine data structure

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -3,6 +3,9 @@ import Alpine from "alpinejs";
 import collapse from "@alpinejs/collapse";
 import "htmx.org";
 
+// Components
+import accordionComponent from './components/accordion';
+
 // Import assets
 import.meta.glob([
   '../images/**',
@@ -19,4 +22,5 @@ declare global {
 // Initialize Alpine
 window.Alpine = Alpine;
 Alpine.plugin(collapse);
+Alpine.data('accordionComponent', accordionComponent);
 Alpine.start();
--- resources/views/components/accordion/index.blade.php
+++ resources/views/components/accordion/index.blade.php
@@ -3,22 +3,7 @@
 <div
   {{ $attributes }}
   role="presentation"
-  x-data="{
-    type: @js($type),
-    open: @js($type === 'single' ? null : []),
-    isOpen(id){ return this.type === 'single' ? this.open === id : this.open.includes(id) },
-    toggle(id){
-      if(this.type === 'single'){ this.open = this.open === id ? null : id }
-      else { this.open = this.isOpen(id) ? this.open.filter(i => i !== id) : [...this.open, id] }
-    },
-    focusList: [],
-    registerTrigger(el){ if (!this.focusList.includes(el)) this.focusList.push(el) },
-    moveFocus(delta, current){
-      const i = this.focusList.indexOf(current); if(i === -1) return;
-      const next = (i + delta + this.focusList.length) % this.focusList.length;
-      this.focusList[next]?.focus();
-    }
-  }"
+  x-data="accordionComponent({ type: @js($type) })"
 >
   {{ $slot }}
 </div>
