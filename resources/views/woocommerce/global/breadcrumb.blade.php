9d778e59dac4c56e36b37b7580363f776dbc0d3a
Ryan Allen <ryansallen53@gmail.com>
2025-09-06 00:45:05 +0100

    feat: add breadcrumb component for WooCommerce to enhance navigation

---
+++ resources/views/woocommerce/global/breadcrumb.blade.php
@@ -0,0 +1,26 @@
+@if (!empty($breadcrumb))
+  @php($count = count($breadcrumb))
+
+  <x-breadcrumbs>
+    @foreach ($breadcrumb as $i => $crumb)
+      @php([$label, $url] = [$crumb[0] ?? '', $crumb[1] ?? ''])
+
+      <x-breadcrumbs.item>
+        @if (!empty($url) && $i + 1 !== $count)
+          <x-breadcrumbs.link :href="esc_url($url)">
+            {{ esc_html($label) }}
+          </x-breadcrumbs.link>
+        @else
+          {{-- Last item (current page) or items without URLs --}}
+          <x-breadcrumbs.page aria-current="page">
+            {{ esc_html($label) }}
+          </x-breadcrumbs.page>
+        @endif
+      </x-breadcrumbs.item>
+
+      @if ($i + 1 !== $count)
+        <x-breadcrumbs.separator />
+      @endif
+    @endforeach
+  </x-breadcrumbs>
+@endif
