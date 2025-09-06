73ab1a20a251b36418ce3992a323e320b430f4ec
Ryan Allen <ryansallen53@gmail.com>
2025-09-06 12:48:05 +0100

    feat: implement order review section in checkout form

--- resources/views/woocommerce/checkout/form-checkout.blade.php
+++ resources/views/woocommerce/checkout/form-checkout.blade.php
@@ -1,7 +1,27 @@
 @extends('woocommerce.checkout.layouts.checkout')
 
 @section('left')
-  left
+  @php
+    do_action('woocommerce_checkout_before_order_review_heading');
+  @endphp
+
+  <h3 class="mb-4 text-2xl font-bold" id="order_review_heading">
+    {{ __('Your order', 'woocommerce') }}
+  </h3>
+
+  @php
+    do_action('woocommerce_checkout_before_order_review');
+  @endphp
+
+  <div id="order_review" class="woocommerce-checkout-review-order">
+    @php
+      do_action('woocommerce_checkout_order_review');
+    @endphp
+  </div>
+
+  @php
+    do_action('woocommerce_checkout_after_order_review');
+  @endphp
 @endsection
 
 @section('right')
