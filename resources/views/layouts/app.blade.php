8618aa0008f9de08e81788d2a4e1a9d404bf8a44
Ryan Allen <ryansallen53@gmail.com>
2025-09-08 09:13:40 +0100

    style: improve HTML formatting and structure in app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,40 +1,40 @@
-<!doctype html>
+<!DOCTYPE html>
 <html @php(language_attributes())>
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     @php(do_action('get_header'))
     @php(wp_head())
 
     @vite(['resources/css/app.css', 'resources/ts/app.ts'])
-</head>
+  </head>
 
-<body @php(body_class(['antialiased']))>
+  <body @php(body_class(['antialiased']))>
     @php(wp_body_open())
     <div id="app">
-        <a class="sr-only focus:not-sr-only" href="#main">
-            {{ __('Skip to content', 'wordpress-quickstart') }}
-        </a>
+      <a class="sr-only focus:not-sr-only" href="#main">
+        {{ __('Skip to content', 'wordpress-quickstart') }}
+      </a>
 
-        @include('sections.header')
+      @include('sections.header')
 
-        <main id="main" class="main">
-            @yield('content')
-        </main>
+      <main id="main" class="main">
+        @yield('content')
+      </main>
 
-        @hasSection('sidebar')
-            <aside class="sidebar">
-                @yield('sidebar')
-            </aside>
-        @endif
+      @hasSection('sidebar')
+        <aside class="sidebar">
+          @yield('sidebar')
+        </aside>
+      @endif
 
-        @include('sections.footer')
+      @include('sections.footer')
     </div>
+
+    <x-toast />
 
     @stack('scripts')
     @php(do_action('get_footer'))
     @php(wp_footer())
-</body>
-
+  </body>
 </html>
