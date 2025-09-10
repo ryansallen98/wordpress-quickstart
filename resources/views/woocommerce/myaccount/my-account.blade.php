5b60ab486c0c71772296e5f635162e3970b758ce
Ryan Allen <ryansallen53@gmail.com>
2025-09-10 15:25:52 +0100

    style: adjust layout and spacing in my account template for better presentation

--- resources/views/woocommerce/myaccount/my-account.blade.php
+++ resources/views/woocommerce/myaccount/my-account.blade.php
@@ -1,9 +1,18 @@
 <div class="relative flex min-h-[100dvh] items-start">
   <div
-    class="bg-sidebar text-sidebar-foreground sticky top-0 min-h-[100dvh] min-w-[16rem] overflow-y-auto border-r p-4 py-8 flex flex-col">
-    @php
-      do_action('woocommerce_account_navigation');
-    @endphp
+    class="bg-sidebar text-sidebar-foreground sticky top-0 min-h-[100dvh] hidden min-w-[16rem] overflow-y-auto border-r xl:flex flex-col">
+
+    {{-- <div class="p-4 lg:p-8 mb-12 border-b">
+      <div class="max-w-[100px] mx-auto">
+
+      </div>
+    </div> --}}
+
+    <div class="p-4 flex-1 flex flex-col">
+      @php
+        do_action('woocommerce_account_navigation');
+      @endphp
+    </div>
   </div>
   <div class="w-full">
     <div class="bg-background sticky top-0 z-10 flex justify-between border-b px-4 py-2">
@@ -14,6 +23,7 @@
     </div>
 
     <div class="p-8">
+      <h1 class="text-2xl font-bold mb-6">{{ $title }}</h1>
       @php
         do_action('woocommerce_account_content');
       @endphp
