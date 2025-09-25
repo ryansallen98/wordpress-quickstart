e233309ec71ce3aa3f17de910d189bdcc36bdab5
Ryan Allen <ryansallen53@gmail.com>
2025-09-25 13:55:01 +0100

    fix: improve formatting and structure in cart template for better readability

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,9 +1,20 @@
 {{--
-  Template Name: Cart
+Template Name: Cart
 --}}
 
 @extends('layouts.app')
 
 @section('content')
+  @php
+    do_action('get_header', 'shop');
+    do_action('woocommerce_before_main_content');
+  @endphp
+
   {!! do_shortcode('[woocommerce_cart]') !!}
+
+  @php
+    do_action('woocommerce_after_main_content');
+    do_action('get_sidebar', 'shop');
+    do_action('get_footer', 'shop');
+  @endphp
 @endsection
