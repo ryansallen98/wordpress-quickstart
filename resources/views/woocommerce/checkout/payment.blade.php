86439ae0fd81d3973857e168c4b41b5de4c15af1
Ryan Allen <ryansallen53@gmail.com>
2025-09-25 13:54:12 +0100

    fix: clean up whitespace and formatting in payment template for consistency

--- resources/views/woocommerce/checkout/payment.blade.php
+++ resources/views/woocommerce/checkout/payment.blade.php
@@ -1,60 +1,54 @@
-@if (! wp_doing_ajax())
-  @php(do_action('woocommerce_review_order_before_payment'))
+@if (!wp_doing_ajax())
+@php(do_action('woocommerce_review_order_before_payment'))
 @endif
 
 <div id="payment" class="woocommerce-checkout-payment mt-8">
   @if (WC()->cart && WC()->cart->needs_payment())
-    <h3 id="payment-methods-heading" class="mb-2 text-xl font-bold">
-      {{ esc_html__('Payment Method', 'woocommerce') }}
-    </h3>
+  <h3 id="payment-methods-heading" class="mb-2 text-xl font-bold">
+    {{ esc_html__('Payment Method', 'woocommerce') }}
+  </h3>
 
-    <ul
-      class="wc_payment_methods payment_methods methods mb-8 flex flex-col gap-2"
-    >
-      @if (! empty($available_gateways))
-        @foreach ($available_gateways as $gateway)
-          @php(wc_get_template('checkout/payment-method.php', ['gateway' => $gateway]))
-        @endforeach
-      @else
-        <li>
-          @php(wc_print_notice(
-            apply_filters(
-              'woocommerce_no_available_payment_methods_message',
-              WC()->customer->get_billing_country()
-                ? __(
-                  'Sorry, it seems that there are no available payment methods. Please contact us if you require assistance or wish to make alternate arrangements.',
-                  'woocommerce',
-                )
-                : __(
-                  'Please fill in your details above to see available payment methods.',
-                  'woocommerce',
-                ),
-            ),
-            'notice',
-          ))
-        </li>
-      @endif
-    </ul>
+  <ul class="wc_payment_methods payment_methods methods mb-8 flex flex-col gap-2">
+    @if (!empty($available_gateways))
+    @foreach ($available_gateways as $gateway)
+    @php(wc_get_template('checkout/payment-method.php', ['gateway' => $gateway]))
+      @endforeach
+    @else
+    <li>
+      @php(wc_print_notice(
+        apply_filters(
+          'woocommerce_no_available_payment_methods_message',
+          WC()->customer->get_billing_country()
+          ? __(
+            'Sorry, it seems that there are no available payment methods. Please contact us if you require assistance or wish to make alternate arrangements.',
+            'woocommerce',
+          )
+          : __(
+            'Please fill in your details above to see available payment methods.',
+            'woocommerce',
+          ),
+        ),
+        'notice',
+      ))
+    </li>
+    @endif
+  </ul>
   @endif
 
   <div class="form-row place-order">
     <noscript>
       {{
-        sprintf(
-          /* translators: 1: <em> 2: </em> */
-          __('Since your browser does not support JavaScript, or it is disabled, please ensure you click the %1$sUpdate Totals%2$s button before placing your order. You may be charged more than the amount stated above if you fail to do so.', 'woocommerce'),
-          '<em>',
-          '</em>',
-        )
+  sprintf(
+    /* translators: 1: <em> 2: </em> */
+    __('Since your browser does not support JavaScript, or it is disabled, please ensure you click the %1$sUpdate Totals%2$s button before placing your order. You may be charged more than the amount stated above if you fail to do so.', 'woocommerce'),
+    '<em>',
+    '</em>',
+  )
       }}
       @php($btnClass = wc_wp_theme_get_element_class_name('button'))
       <br />
-      <button
-        type="submit"
-        class="button alt{{ $btnClass ? ' ' . $btnClass : '' }}"
-        name="woocommerce_checkout_update_totals"
-        value="{{ esc_attr__('Update totals', 'woocommerce') }}"
-      >
+      <button type="submit" class="button alt{{ $btnClass ? ' ' . $btnClass : '' }}"
+        name="woocommerce_checkout_update_totals" value="{{ esc_attr__('Update totals', 'woocommerce') }}">
         {{ __('Update totals', 'woocommerce') }}
       </button>
     </noscript>
@@ -65,18 +59,18 @@
 
     {{-- Filter returns full HTML for the Place Order button --}}
     {!!
-      apply_filters(
-        'woocommerce_order_button_html',
-        '<button type="submit" class="btn btn-primary btn-lg alt' .
-          esc_attr(($btnClass = wc_wp_theme_get_element_class_name('button')) ? ' ' . $btnClass : '') .
-          '" name="woocommerce_checkout_place_order" id="place_order" value="' .
-          esc_attr($order_button_text) .
-          '" data-value="' .
-          esc_attr($order_button_text) .
-          '">' .
-          esc_html($order_button_text) .
-          '</button>',
-      )
+  apply_filters(
+    'woocommerce_order_button_html',
+    '<button type="submit" class="btn btn-primary btn-lg alt' .
+    esc_attr(($btnClass = wc_wp_theme_get_element_class_name('button')) ? ' ' . $btnClass : '') .
+    '" name="woocommerce_checkout_place_order" id="place_order" value="' .
+    esc_attr($order_button_text) .
+    '" data-value="' .
+    esc_attr($order_button_text) .
+    '">' .
+    esc_html($order_button_text) .
+    '</button>',
+  )
     !!}
 
     @php(do_action('woocommerce_review_order_after_submit'))
@@ -85,6 +79,6 @@
   </div>
 </div>
 
-@if (! wp_doing_ajax())
-  @php(do_action('woocommerce_review_order_after_payment'))
+@if (!wp_doing_ajax())
+@php(do_action('woocommerce_review_order_after_payment'))
 @endif
