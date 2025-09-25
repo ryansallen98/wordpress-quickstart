8ddd21d80dd6f8decf3c07b604027f3187ddecac
Ryan Allen <ryansallen53@gmail.com>
2025-09-25 13:55:41 +0100

    feat: disable Gutenberg editor for pages using the Flexible Content template

--- app/filters.php
+++ app/filters.php
@@ -15,7 +15,21 @@ add_filter('excerpt_more', function () {
     return sprintf(' &hellip; <a href="%s">%s</a>', get_permalink(), __('Continued', 'wordpress-quickstart'));
 });
 
+/**
+ * Disable Gutenberg editor for pages using the "Flexible Content" template.
+ */
+add_filter( 'use_block_editor_for_post', function ( $use_block_editor, $post ) {
+    if ( ! $post ) {
+        return $use_block_editor;
+    }
 
-add_filter( 'loop_shop_columns', function() {
-    return 6; // change to 2, 3, 4, etc.
-}, 20 );
+    if ( $post->post_type === 'page' ) {
+        $template = get_page_template_slug( $post );
+
+        if ( $template === 'flexible.blade.php' || $template === 'flexible.php' ) {
+            return false; // disable block editor for this template
+        }
+    }
+
+    return $use_block_editor;
+}, 10, 2 );
